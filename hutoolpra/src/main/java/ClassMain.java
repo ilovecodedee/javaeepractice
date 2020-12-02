7ecc3f9267c996fcb6139ffac9acbd7394153e60
xhl <xhl@qq.com>
2020-12-02 21:30:35 +0800

    还原

--- hutoolpra/src/main/java/ClassMain.java
+++ hutoolpra/src/main/java/ClassMain.java
@@ -4,14 +4,6 @@ import java.util.Properties;
 
 public class ClassMain {
     public static void main(String[] args) throws IOException {
-         Properties props = new Properties();
-        FileInputStream in = new FileInputStream("C:/log/log/config.properties");
-
-
-        props.load(in);
-        String name = props.getProperty("name");
-        String age = props.getProperty("age");
-        System.out.println(name);
-        System.out.println(age);
+        System.out.println("hello hutool");
     }
 }
